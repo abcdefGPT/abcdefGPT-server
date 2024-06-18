b8e6280f1ffb1936f124809b82bbcad878e2dc17
bbangjun <lucid9dawn@gmail.com>
2024-06-18 23:51:15 +0900

    perf: 서버 실행 시 초기 쿼리에 대한  llm_chain을 미리 구축함으로써  API 성능 개선

--- llm/prototype_rag.py
+++ llm/prototype_rag.py
@@ -6,26 +6,7 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain import hub
 import json
 
-
-
-def run(query, vectorstore, llm):
-    # Decomposition Template
-    template = (
-            "You need to get multiple single questions by performing decomposition to make it easier to search for multiple documents from complex questions." +
-            "The conditions are as follows. Please make sure that all conditions are met." +
-            "1. The {Query} I gave you means a complex query, and the result of decomposing Q is a single query (SQ). There can be multiple SQs (SQ1, SQ2, SQ3...)" +
-            "2. Decomposition should be as consistent as possible (the same complex question should be given)" +
-            "3. The decomposed SQ should be generated based on the common main entity in Q." +
-            "Examples of satisfying all conditions are as follows." +
-            "Q: Who is older, Annie Morton or Terry Richardson? SQ1: How old is Annie Morton? SQ2: How old is Terry? SQ1: How old is Annie Morton? SQ2: How old is Terry Richardson?" +
-            "Q: Was there no change in the portrayal of Google's influence on the digital ecosystem between the report from The Verge on Google's impact on the internet's appearance published on November 1, 2023, and the report from TechCrunch on a class action antitrust suit against Google published later? SQ1: What is the portrayal of Google's influence on the digital ecosystem in the report published by The Verge on Google's impact on the internet's appearance on November 1, 2023? SQ2: What is the portrayal of Google's influence on the digital ecosystem in the report published by TechCrunch on a class action antitrust suit against Google after November 1, 2023?" +
-            "4. For example, if you have the following Q and SQs, SQ3 is not the SQ for retrieving the document. Since these SQs must be handled by llm, it is appropriate not to be the result of query decomposition." +
-            "Q: Does 'The New York Times' article suggest that Connor Bedard has the potential to dominate in the NHL, while the 'Sporting News' article indicates that the USC basketball team has the potential to become a National Championship contender, or do both articles suggest a similar potential for their respective subjects? SQ1: What does 'The New York Times' article say about Connor Bedard's potential to dominate in the NHL? SQ2: What does the 'Sporting News' article say about the USC basketball team's potential to become a National Championship contender? SQ3: Do both articles suggest a similar potential for their respective subjects?" +
-            "5. SQs should have a high similarity to Q." +
-            "6. Give me Q and SQs only JSON form, and don't use another annotation. I dont need answer about SQs")
-
-    prompt1 = PromptTemplate(template=template, input_variables=['Query'])
-    llm_chain = prompt1 | llm
+def run(query, vectorstore, llm, llm_chain):
 
     QUERY = "Q: " + query
     Answer = llm_chain.invoke(QUERY)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from langchain_core.prompts import PromptTemplate
 from sqlalchemy.future import select
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -14,6 +15,30 @@ load_dotenv()
 GPT_API_KEY = os.getenv("GPT_API_KEY")
 os.environ["OPENAI_API_KEY"] = GPT_API_KEY
 app = FastAPI()
+llm = ChatOpenAI(
+    temperature=1.0,  # 창의성 (0.0 ~ 2.0)
+    max_tokens=2048,  # 최대 토큰수
+    model_name='gpt-4o',  # 모델명
+)
+
+# Decomposition Template
+template = (
+        "You need to get multiple single questions by performing decomposition to make it easier to search for multiple documents from complex questions." +
+        "The conditions are as follows. Please make sure that all conditions are met." +
+        "1. The {Query} I gave you means a complex query, and the result of decomposing Q is a single query (SQ). There can be multiple SQs (SQ1, SQ2, SQ3...)" +
+        "2. Decomposition should be as consistent as possible (the same complex question should be given)" +
+        "3. The decomposed SQ should be generated based on the common main entity in Q." +
+        "Examples of satisfying all conditions are as follows." +
+        "Q: Who is older, Annie Morton or Terry Richardson? SQ1: How old is Annie Morton? SQ2: How old is Terry? SQ1: How old is Annie Morton? SQ2: How old is Terry Richardson?" +
+        "Q: Was there no change in the portrayal of Google's influence on the digital ecosystem between the report from The Verge on Google's impact on the internet's appearance published on November 1, 2023, and the report from TechCrunch on a class action antitrust suit against Google published later? SQ1: What is the portrayal of Google's influence on the digital ecosystem in the report published by The Verge on Google's impact on the internet's appearance on November 1, 2023? SQ2: What is the portrayal of Google's influence on the digital ecosystem in the report published by TechCrunch on a class action antitrust suit against Google after November 1, 2023?" +
+        "4. For example, if you have the following Q and SQs, SQ3 is not the SQ for retrieving the document. Since these SQs must be handled by llm, it is appropriate not to be the result of query decomposition." +
+        "Q: Does 'The New York Times' article suggest that Connor Bedard has the potential to dominate in the NHL, while the 'Sporting News' article indicates that the USC basketball team has the potential to become a National Championship contender, or do both articles suggest a similar potential for their respective subjects? SQ1: What does 'The New York Times' article say about Connor Bedard's potential to dominate in the NHL? SQ2: What does the 'Sporting News' article say about the USC basketball team's potential to become a National Championship contender? SQ3: Do both articles suggest a similar potential for their respective subjects?" +
+        "5. SQs should have a high similarity to Q." +
+        "6. Give me Q and SQs only JSON form, and don't use another annotation. I dont need answer about SQs")
+
+prompt1 = PromptTemplate(template=template, input_variables=['Query'])
+llm_chain = prompt1 | llm
+
 
 @app.on_event("startup")
 async def on_startup():
@@ -29,6 +54,7 @@ async def create_vector_store():
     app.state.vectorstore = vectorstore
 
 
+
 async def get_db():
     async with AsyncSessionLocal() as db:
         yield db
@@ -37,13 +63,9 @@ async def get_db():
 @app.get("/")
 async def read_root(query: str):
     try:
-        llm = ChatOpenAI(
-            temperature=1.0,  # 창의성 (0.0 ~ 2.0)
-            max_tokens=2048,  # 최대 토큰수
-            model_name='gpt-4o',  # 모델명
-        )
 
-        answer = prototype_rag.run(query, app.state.vectorstore, llm)  # 벡터스토어를 함수로 전달
+
+        answer = prototype_rag.run(query, app.state.vectorstore, llm, llm_chain)  # 벡터스토어를 함수로 전달
         return {"answer": answer}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
